af34d64c7cb70f637278eb26ecc455b640e78867
Olivier Bossel <olivier.bossel@gmail.com>
2018-05-15 15:19:55 +0200

    update tests + some packages

--- dist/class.js
+++ dist/class.js
@@ -1,11 +1,13 @@
+'use strict';
+
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
-var _STemplateComponent = require('./js/STemplateComponent');
+var _Component = require('./js/Component');
 
-var _STemplateComponent2 = _interopRequireDefault(_STemplateComponent);
+var _Component2 = _interopRequireDefault(_Component);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-exports.default = _STemplateComponent2.default;
+exports.default = _Component2.default;
