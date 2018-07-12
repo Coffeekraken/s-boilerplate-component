ef7aef20cc589bc0211056ca242241432e3213dc
Olivier Bossel <olivier.bossel@gmail.com>
2018-07-12 16:16:15 +0200

    clean some things

--- code-playground.config.js
+++ code-playground.config.js
@@ -1,60 +1,55 @@
 module.exports = {
-	// server port
-	port : 3000,
+  // server port
+  port: 3000,
 
-	// title
-	title : 's-{component-name}-component',
+  // title
+  title: 's-{component-name}-component',
 
-	// layout
-	layout : 'right',
+  // layout
+  layout: 'right',
 
-	// compile server
-	compileServer : {
+  // compile server
+  compileServer: {
 
-		// compile server port
-		port : 4000
+    // compile server port
+    port: 4000
 
-	},
+  },
 
-	// editors
-	editors : {
-		html : {
-			language : 'html',
-			data : `
-				<div class="container">
-					<h1 class="h1 m-b-small">
-						Coffeekraken s-{component-name}-component
-					</h1>
-					<p class="p m-b-bigger">
-						{component-description}
-					</p>
-					<!-- examples here... -->
-				</div>
-			`
-		},
-		css : {
-			language : 'sass',
-			data : `
-				@import 'node_modules/coffeekraken-sugar/index';
-				@include s-init();
-				@include s-classes();
-				@include s-typography-classes();
-				body {
-					background-image: linear-gradient(120deg, #fdfbfb 0%, #ebedee 100%);
-				}
-				.container {
-					@include s-position(absolute, middle, center);
-					min-width:80vw;
-				}
-				// component css here...
-			`
-		},
-		js : {
-			language : 'js',
-			data : `
-				import 'webcomponents.js/webcomponents-lite'
-				import Component from './dist/index'
-			`
-		}
-	}
+  // editors
+  editors: {
+    html: {
+      language: 'html',
+      data: `
+        <h1 class="h3 m-b-small">
+          Coffeekraken s-{component-name}-component
+        </h1>
+        <p class="p m-b-bigger">
+          {component-description}
+        </p>
+        <!-- examples here... -->
+      `
+    },
+    css: {
+      language: 'sass',
+      data: `
+        @import 'node_modules/coffeekraken-sugar/index';
+        @import 'node_modules/coffeekraken-s-typography-component/index';
+        @include s-init();
+        @include s-classes();
+        @include s-typography-classes();
+        body {
+          padding: s-space(bigger);
+        }
+        // component css here...
+      `
+    },
+    js: {
+      language: 'js',
+      data: `
+        import 'webcomponents.js/webcomponents-lite'
+        import Component from './dist/index'
+      `
+    }
+  }
 }
